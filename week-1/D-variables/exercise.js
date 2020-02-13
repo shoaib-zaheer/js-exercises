64f649701f6156a3a7e3f9cb7a14f6e88ac90ea2
“Shoaib-zaheer” <z.shoaib015@gmail.com>
2020-02-13 14:51:55 +0100

    strings and fstrings-concaten

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,7 @@
 // Start by creating a variable `greeting`
-
+var greeting = "Hello for Nothing";
+var greetings = "Hello for Something";
+var greet = "Hello for Question Mark";
 console.log(greeting);
+console.log(greetings);
+console.log(greet);
